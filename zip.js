00735f2e84cf6d68c40d8c67637aeffe30e0c90f
Ria Verma <riaverma96@outlook.com>
2016-09-24 07:50:40 -0400

    Angular implementation.

--- zip.js
+++ zip.js
@@ -4,23 +4,3 @@ month = (month * 1) + 1
 day = test.getDate()
 year = test.getFullYear()
 document.write(" ",month,"/",day,"/",year," ")
-
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("get", url, true);
-    xhr.responseType = "json";
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status);
-      }
-    };
-    xhr.send();
-};
-
-getJSON('congress.api.sunlightfoundation.com/legislators/locate?zip=30092&apikey=f7de381a2c9040249a24a93ce8b88c67', function (data)
-    {
-        console.log(data[0].first_name);
-    });
